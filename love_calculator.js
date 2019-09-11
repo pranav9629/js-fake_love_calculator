051a90dbfc36d995d2aa4eef13f841c920aa5d2a
Pranav <46915136+pranav9629@users.noreply.github.com>
2019-09-12 00:59:55 +0530

    Add files via upload

---
+++ love_calculator.js
@@ -0,0 +1,5 @@
+// Love calcultor module that shows a random percentage value
+var name1 = prompt("Enter your name: ");
+var name2 = prompt("Enter the name of the person of whom you wish to calculate love percentage: ");
+var random_number = Math.round((Math.random())*100);
+alert(name1 + " and " + name2 + " shares a love percentage of " + random_number + "%");
